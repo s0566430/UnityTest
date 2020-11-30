e62e1d0a8a5b0781ebd5f98750da9d2b20ee3378
s0566430 <s0566430@htw-berlin.de>
2020-11-30 20:08:48 +0100

    imported dots + hybrid renderer, added PlayerComponent + PlayerBehaviorSystem

---
+++ Assets/Scripts/PlayerComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct PlayerComponent : IComponentData
+{
+    public float speed;
+    public float rotationAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
